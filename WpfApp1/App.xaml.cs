2a674d42e8fbd1ab3fcc522495999317e93ccb3f
demillll <bykbyk20030508@gmail.com>
2025-01-26 11:48:59 +0300

    исправлены ошибки с платформами подчищены остатки main в настройках проекта

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -8,10 +8,11 @@ using System.Windows;
 
 namespace WpfApp1
 {
-	/// <summary>
-	/// Логика взаимодействия для App.xaml
-	/// </summary>
 	public partial class App : Application
 	{
+		public App()
+		{
+			this.InitializeComponent();
+		}
 	}
 }
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Windows;
+using LineRaceGame;
 
 namespace WpfApp1
 {
